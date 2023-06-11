148323c6774a0295f56d2d4d8e83d84c826a2d0b
Diego Vicente <dvicentejua@gmail.com>
2023-06-11 18:09:16 +0200

    Se anade gastoCliente como atributo a comercio

--- src/main/java/bestprice/libreria/Online.java
+++ src/main/java/bestprice/libreria/Online.java
@@ -3,5 +3,6 @@ package bestprice.libreria;
 public interface Online {
 	float getEnvio();
 	float getMinimo();
+	float getGastoCliente();
 
 }
--- src/main/java/bestprice/libreria/OnlineImpl.java
+++ src/main/java/bestprice/libreria/OnlineImpl.java
@@ -8,6 +8,7 @@ public class OnlineImpl extends ComercioImpl implements Online {
 	
 	private float envio;
 	private float minimo;
+	private float gastoCliente;
 	
 	@Override
 	public float getEnvio() {
@@ -23,10 +24,18 @@ public class OnlineImpl extends ComercioImpl implements Online {
 	public void setMinimo(float minimo) {
 		this.minimo = minimo;
 	}
+		
+	
+	public float getGastoCliente() {
+		return gastoCliente;
+	}
+	public void setGastoCliente(float gastoCliente) {
+		this.gastoCliente = gastoCliente;
+	}
 	@Override
 	public String toString() {
 		return super.toString()+"OnlineImpl [getTipoComercio()=" + getTipo() + ", getEnvio()=" + getEnvio() + ", getMinimo()="
-				+ getMinimo() + "]";
+				+ getMinimo() + "getGastoCliente()="+getGastoCliente()+ "]";
 	}
 	
 	
